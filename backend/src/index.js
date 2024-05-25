426042e4275c36da18908232d5a630c2f04ec9e7
Inga Viitanen <ingaa134@gmail.com>
2024-05-25 18:44:29 +0200

    login

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const serverClient = StreamChat.getInstance(config.api_key, config.api_secret)
 
 app.post("/signup", async (req, res) => {
     try {
-        const {firstName, lastName, username, password} = req.body
+        const { firstName, lastName, username, password } = req.body
         const userId = uuidv4()
         const hashedPass = await bcrypt.hash(password, 10)
         const token = serverClient.createToken(userId)
@@ -24,8 +24,23 @@ app.post("/signup", async (req, res) => {
         res.json(error)
     }
 })
-app.post("/login")
- 
+app.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const { users } = await serverClient.queryUsers({ name: username })
+        if (users.length === 0) return res.json({ message: "User not found" })
+    
+        const token = serverClient.createToken(userId)
+        const passwordMatch = await bcrypt.compare(password, users[0].hashedPass)
+    
+        if (passwordMatch) {
+            res.json({ token, firstName: users[0].firstName, lastName: users[0].lastName, username, userId: users[0].userId })
+        }
+    } catch (error) {
+        res.json(error)
+    }
+})
+
 app.listen(4000, () => {
     console.log('server is running on port 4000')
 })
